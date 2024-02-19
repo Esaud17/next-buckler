090634e931ac5aabe4675fc2a517ea391b6aa37d
Esaud Rivera Lagos <Esaud17@users.noreply.github.com>
2024-02-18 20:48:40 -0600

    update  version and rename

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { BucklerGuard } from './guard/buckler-guard'
-export { BucklerStrategy } from './guard/buckler-strategy'
+export { BucklerStrategy as Buckler } from './guard/buckler-strategy'
 export { BucklerProps } from './types/properties/buckler-props'
